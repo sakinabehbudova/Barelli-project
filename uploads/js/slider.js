69c9030ea9630c922870445c0f68c2084795590a
sakinabehbudova <sakina.behbudova1996@gmail.com>
2021-10-15 11:30:39 +0400

    add contact page

--- uploads/js/slider.js
+++ uploads/js/slider.js
@@ -12,3 +12,31 @@
             var slideno = $(this).data('slide');
             $('.slider-nav').slick('slickGoTo', slideno - 1);
         });
+
+        /*slider two*/
+        jQuery(document).ready(function($) {
+            "use strict";
+            $('#customers-testimonials').owlCarousel( {
+                    loop: true,
+                    center: true,
+                    items: 5,
+                    margin: 30,
+                    autoplay: true,
+                    dots:true,
+                nav:true,
+                    autoplayTimeout: 8500,
+                    smartSpeed: 450,
+                  navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+                    responsive: {
+                        0: {
+                            items: 1
+                        },
+                        768: {
+                            items: 2
+                        },
+                        1170: {
+                            items: 5
+                        }
+                    }
+                });
+            });
