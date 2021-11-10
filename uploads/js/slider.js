6c2a4dab9410a42ab46798c6a633ffefb566cfd5
sakinabehbudova <sakina.behbudova1996@gmail.com>
2021-11-10 11:06:52 +0400

    improvements on design

--- uploads/js/slider.js
+++ uploads/js/slider.js
@@ -4,7 +4,35 @@
             slidesToScroll: 1,
             asNavFor: '.slider-for',
             dots: true,
-            focusOnSelect: true
+            focusOnSelect: true,
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
         });
 
         $('a[data-slide]').click(function (e) {
@@ -12,6 +40,61 @@
             var slideno = $(this).data('slide');
             $('.slider-nav').slick('slickGoTo', slideno - 1);
         });
+
+
+
+
+
+        $('.responsive').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
+          });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         /*slider two*/
         jQuery(document).ready(function($) {
@@ -23,7 +106,7 @@
                     margin: 30,
                     autoplay: true,
                     dots:true,
-                nav:true,
+                     nav:true,
                     autoplayTimeout: 8500,
                     smartSpeed: 450,
                   navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
